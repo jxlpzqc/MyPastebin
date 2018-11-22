3e0818ae3c63d37372fc1f15e17e28250e39279c
An Honest Liar <jxlpzqc@qq.com>
2018-11-22 15:58:14 +0800

    basically finish the function

--- web.py
+++ web.py
@@ -3,8 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from time import time
 import random,hashlib
 
+#数据库配置
+SQL_USERNAME = 'root'
+SQL_PASSWORD = '6666'
+SQL_HOSTADDRESS = 'localhost'
+SQL_HOSTPORT = '3306'
+SQL_DATABASE = 'pastebin'
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:6666@localhost:3306/pastebin'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + SQL_USERNAME + ':' + SQL_PASSWORD + '@' + SQL_HOSTADDRESS + ':' + SQL_HOSTPORT + '/' + SQL_DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
@@ -36,6 +43,12 @@ syntaxs = [ {'id':1,'text':'pain text' },{'id':2,'text':'Python' } ]
 def index():
 	return render_template('main.html',syntaxs=syntaxs)
 
+@app.route('/p/<id>')
+def show(id):
+	message = Message.query.filter_by(id=id).first()
+	return render_template('show.html',message=message)
+
+	
 @app.route('/new',methods = ['POST'])
 def	newpaste():
 	poster = request.form.get('poster')
@@ -48,12 +61,11 @@ def	newpaste():
 	md5 = hashlib.md5()
 	md5.update(idsrc.encode('utf-8'))
 	id = md5.hexdigest()
-	
 	new = Message(id,poster,syntax,content,expiration)
 	db.session.add(new)
 	db.session.commit()
 	
-	return 	id
+	return redirect(url_for('show',id=id))
 	
 if __name__ == '__main__':
 	#db.drop_all()
